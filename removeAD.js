1d02d595dcaf1c6989ef9897d56a32c051a139c7
虞金攀 <1192878390@qq.com>
2018-11-16 00:13:17 +0800

    docs: 更新头部信息

--- removeAD.js
+++ removeAD.js
@@ -1,15 +1,14 @@
 // ==UserScript==
-// @name         电影天堂去掉广告
-// @namespace    http://tampermonkey.net/
-// @version      0.1
-// @description  shows how to use babel compiler
-// @author       You
+// @name         电影天堂去掉广告与高亮高分电影
+// @namespace    https://greasyfork.org/zh-CN/users/226081-yujinpan
+// @version      0.2
+// @description  电影天堂网站去掉广告与高亮高分电影
+// @author       yujinpan
 // @require      https://cdnjs.cloudflare.com/ajax/libs/babel-standalone/6.18.2/babel.js
 // @require      https://cdnjs.cloudflare.com/ajax/libs/babel-polyfill/6.16.0/polyfill.js
 // @match        www.dytt8.net/*
 // ==/UserScript==
 
-// Your code here...
 (function () {
 
     // 功能1：删除第一次加载出现的广告

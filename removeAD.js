e8567f97908bc756751f3f5820440b7b1eb6a4c5
yujinpan <1192878390@qq.com>
2018-11-16 17:29:19 +0800

    chore(removeAD): 去掉不必要的es6语法

--- removeAD.js
+++ removeAD.js
@@ -1,12 +1,10 @@
 // ==UserScript==
 // @name         电影天堂(www.dytt8.net)去掉广告与高亮高分电影
 // @namespace    https://github.com/yujinpan/tampermonkey-extension
-// @version      0.4
+// @version      0.5
 // @license      MIT
 // @description  提高电影天堂(www.dytt8.net)网站浏览体验与效率。
 // @author       yujinpan
-// @require      https://cdnjs.cloudflare.com/ajax/libs/babel-standalone/6.18.2/babel.js
-// @require      https://cdnjs.cloudflare.com/ajax/libs/babel-polyfill/6.16.0/polyfill.js
 // @match        www.dytt8.net/*
 // ==/UserScript==
 
@@ -23,18 +21,18 @@
 // 功能5：删除右下角的flash广告
 (function () {
     // 生成广告列表实例
-    const ads = new Ads(['link', 'flash']);
+    var ads = new Ads(['link', 'flash']);
 
     // 循环搜索广告
-    let adSearchCounter = 0;
+    var adSearchCounter = 0;
     (function removeAd() {
         adSearchCounter++;
-        console.log(`第${adSearchCounter}次查找广告。`);
+        console.log('第' + adSearchCounter + '次查找广告。');
 
-        const elems = document.body.children;
+        var elems = document.body.children;
 
         // 查找全屏链接广告
-        const link = elems[0];
+        var link = elems[0];
         if (ads.get('link') && link && link.nodeName === 'A' && link.href) {
             link.remove();
             ads.remove('link');
@@ -42,7 +40,7 @@
         }
 
         // 查找右下角flash窗口
-        const flash = elems[elems.length - 1];
+        var flash = elems[elems.length - 1];
         if (ads.get('flash') && flash && flash.style.position === 'fixed') {
             flash.remove();
             ads.remove('flash');
@@ -68,9 +66,9 @@
      * @method get(adName) 获取广告是否移除，默认无参数返回广告的总数量
      */
     function Ads(ads) {
-        let _ads = {};
-        let _adsLen = 0;
-        ads.forEach(ad => {
+        var _ads = {};
+        var _adsLen = 0;
+        ads.forEach(function (ad) {
             _ads[ad] = true;
             _adsLen++;
         });
@@ -96,34 +94,36 @@
 
 // 功能2：标记高分电影
 (function () {
-    const markWords = ['高分', '获奖'];
-    const allText = document.querySelectorAll('a, p');
-    const textMark = (text, markWords) => {
-        markWords.forEach(word => text = text.replace(new RegExp(word), '<strong style="color: red;font-size: 18px;">' + word + '</strong>'));
+    var markWords = ['高分', '获奖'];
+    var allText = document.querySelectorAll('a, p');
+    var textMark = function (text, markWords) {
+        markWords.forEach(function (word) {
+            text = text.replace(new RegExp(word), '<strong style="color: red;font-size: 18px;">' + word + '</strong>');
+        });
         return text;
     };
-    allText.forEach(aElem => {
+    allText.forEach(function (aElem) {
         aElem.innerHTML = textMark(aElem.innerHTML, markWords);
     });
 })();
 
 // 功能3：去掉搜索框的广告跳转
 (function () {
-    document.querySelector('input[name="keyword"]').addEventListener('keydown', (e) => { e.stopPropagation(); });
+    document.querySelector('input[name="keyword"]').addEventListener('keydown', function (e) { e.stopPropagation(); });
 })();
 
 // 功能4：去掉中间页面上的flash广告
 // (还可以提高网站性能，不影响页面布局)
 (function () {
-    const containWidth = document.querySelector('.contain').clientWidth;
+    var containWidth = document.querySelector('.contain').clientWidth;
     // 递归移除父级，除与广告的尺寸不一致就停止
     // 误差范围宽度30px，高度10px
-    const removeParentUntilDiffSize = (elem) => {
-        const elemW = elem.clientWidth;
-        const elemH = elem.clientHeight;
-        const parent = elem.parentNode;
-        const parentW = parent.clientWidth;
-        const parentH = parent.clientHeight;
+    var removeParentUntilDiffSize = function (elem) {
+        var elemW = elem.clientWidth;
+        var elemH = elem.clientHeight;
+        var parent = elem.parentNode;
+        var parentW = parent.clientWidth;
+        var parentH = parent.clientHeight;
         if (checkSize(parentW, elemW, 30) && checkSize(parentH, elemH, 10)) {
             removeParentUntilDiffSize(parent);
         } else {
@@ -131,10 +131,10 @@
         }
     };
     // 校验大小是否在误差之内
-    const checkSize = (current, target, range) => {
+    var checkSize = function (current, target, range) {
         return current > target - 30 && current < target + 30;
     }
-    document.querySelectorAll('iframe').forEach(elem => {
+    document.querySelectorAll('iframe').forEach(function (elem) {
         if (elem.clientWidth === containWidth) {
             removeParentUntilDiffSize(elem);
         } else {

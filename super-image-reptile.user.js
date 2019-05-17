e6536acf500ce34d2a9f020972ed8f3d907771f8
yujinpan <1192878390@qq.com>
2019-05-17 13:27:42 +0800

    refactor: 重构核心代码，修复svg，canvas 的预览与下载问题，增加暗黑透明样式

--- super-image-reptile.user.js
+++ super-image-reptile.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         网站图片（背景图，svg，canvas）抓取预览下载
 // @namespace    https://github.com/yujinpan/tampermonkey-extension
-// @version      1.0
+// @version      2.0
 // @license      MIT
 // @description  将站点所有的图片（背景图，svg，canvas）抓取提供预览，直接点击下载。
 // @author       yujinpan
@@ -15,213 +15,210 @@
  * - 提供按钮快速切换抓取的图片展示区
  * - 提供快速下载，点击预览即可下载源图片文件
  * - 提供动态抓取后来加载的图片
+ * 
+ * 2019-5-17 更新内容：
+ * - 修复 svg，canvas 展示与下载问题
+ * - 增加暗黑透明样式，黑色，白色图片区分明显
+ * - 重构核心代码，分模块执行，提高可读性与维护性
  */
 
-// 抓取的图片
-const urls = new Set();
-const svgs = new Set();
-const canvas = new Set();
+(() => {
+  // 存放抓取与生成的图片
+  const urls = new Set();
 
-showImageList();
+  // 初始化
+  init();
 
-// 获取资源列表
-function imagesReptile() {
-  const elements = Array.from(document.querySelectorAll('*'));
+  /**
+   * 初始化
+   */
+  function init() {
+    // 创建样式
+    createStyle();
 
-  // 遍历取出 img，backgroundImage，svg，canvas
-  for (const element of elements) {
-    const tagName = element.tagName.toLowerCase();
+    // 创建容器
+    const section = document.createElement('section');
+    section.id = 'SIR';
+    section.innerHTML = `
+      <button class="SIR-toggle-button">自动获取图片</button>
+      <ul class="SIR-main">
+      </ul>
+    `;
+    document.body.append(section);
 
-    // img 标签
-    if (tagName === 'img') {
-      urls.add(getImgUrl(element));
-      continue;
-    }
+    // 获取按钮与列表 DOM
+    const button = section.querySelector('.SIR-toggle-button');
+    const main = section.querySelector('.SIR-main');
 
-    // svg
-    if (tagName === 'svg') {
-      svgs.add(element);
-      continue;
-    }
+    // 切换时进行抓取
+    let showMain = false;
+    button.onclick = () => {
+      showMain = !showMain;
+      if (showMain) {
+        imagesReptile();
+        // content
+        let imageList = '';
+        urls.forEach((url) => {
+          imageList += `
+            <li>
+              <a download="image" title="点击下载" href="${url}">
+                <img src='${url}' />
+              </a>
+            </li>`;
+        });
+        main.innerHTML = imageList;
+      } else {
+        main.innerHTML = '';
+      }
+      main.style.display = showMain ? 'flex' : 'none';
+    };
+  }
 
-    // canvas
-    if (tagName === 'canvas') {
-      canvas.add(element);
-      continue;
-    }
+  /**
+   * 获取资源列表
+   */
+  function imagesReptile() {
+    const elements = Array.from(document.querySelectorAll('*'));
 
-    // background-image
-    const backgroundImage = getComputedStyle(element).backgroundImage;
-    if (backgroundImage !== 'none' && backgroundImage.startsWith('url')) {
-      urls.add(backgroundImage.slice(5, -2));
+    // 遍历取出 img，backgroundImage，svg，canvas
+    for (const element of elements) {
+      const tagName = element.tagName.toLowerCase();
+
+      // img 标签
+      if (tagName === 'img') {
+        urls.add(getImgUrl(element));
+        continue;
+      }
+
+      // svg
+      if (tagName === 'svg') {
+        urls.add(getSvgImage(element));
+        continue;
+      }
+
+      // canvas
+      if (tagName === 'canvas') {
+        urls.add(getCanvasImage(element));
+        continue;
+      }
+
+      // background-image
+      const backgroundImage = getComputedStyle(element).backgroundImage;
+      if (backgroundImage !== 'none' && backgroundImage.startsWith('url')) {
+        urls.add(backgroundImage.slice(5, -2));
+      }
     }
   }
-}
 
-// 创建展示列表
-function showImageList() {
-  // style
-  const style = document.createElement('style');
-  style.innerHTML = `
-    .SIR-toggle-button {
-        position: fixed;
-        right: 0;
-        bottom: 0;
-        z-index: 9999;
-        opacity: 0.5;
-    }
-    .SIR-toggle-button:hover {
-      opacity: 1;
-    }
-    .SIR-main {
-        position: fixed;
-        width: 100%;
-        height: 100%;
-        top: 0;
-        left: 0;
-        z-index: 9998;
-        overflow-y: auto;
-        margin: 0;
-        padding: 0;
-        list-style-type: none;
-        display: none;
-        flex-wrap: wrap;
-        text-align: center;
-        min-height: 100%;
-        background: #eee;
-    }
-    .SIR-main > li {
-        box-sizing: border-box;
-        width: 10%;
-        min-height: 100px;
-        max-height: 200px;
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        padding: 1px;
-        border: 1px solid #ddd;
-        cursor: pointer;
-        background: inherit;
-        overflow: hidden;
-    }
-    .SIR-main > li:hover > img,
-    .SIR-main > li:hover > svg,
-    .SIR-main > li:hover > canvas {
-        transform: scale(1.5);
-    }
-    .SIR-main > li > img,
-    .SIR-main > li > svg,
-    .SIR-main > li > canvas {
-        transition: transform .3s;
-        max-width: 100%;
-        max-height: 100%;
-    }
-  `;
+  /**
+   * 创建样式
+   */
+  function createStyle() {
+    const style = document.createElement('style');
+    style.innerHTML = `
+      .SIR-toggle-button {
+          position: fixed;
+          right: 0;
+          bottom: 0;
+          z-index: 99999;
+          opacity: 0.5;
+      }
+      .SIR-toggle-button:hover {
+        opacity: 1;
+      }
+      .SIR-main {
+          position: fixed;
+          width: 100%;
+          height: 100%;
+          top: 0;
+          left: 0;
+          z-index: 99998;
+          overflow-y: auto;
+          margin: 0;
+          padding: 0;
+          list-style-type: none;
+          display: none;
+          flex-wrap: wrap;
+          text-align: center;
+          min-height: 100%;
+          background: rgba(0, 0, 0, 0.8);
+      }
+      .SIR-main > li {
+          box-sizing: border-box;
+          width: 10%;
+          min-height: 100px;
+          max-height: 200px;
+          padding: 1px;
+          box-shadow: 0 0 1px darkgrey;
+          background: rgba(0, 0, 0, 0.5);
+          overflow: hidden;
+      }
+      .SIR-main > li > a {
+          display: flex;
+          justify-content: center;
+          align-items: center;
+          width: 100%;
+          height: 100%;
+      }
+      .SIR-main > li:hover img {
+          transform: scale(1.5);
+      }
+      .SIR-main > li img {
+          transition: transform .3s;
+          max-width: 100%;
+      }
+    `;
+    document.head.append(style);
+  }
 
-  // container
-  const section = document.createElement('section');
-  section.id = 'SIR';
-  section.innerHTML = `
-    <button class="SIR-toggle-button">自动获取图片</button>
-    <ul title="点击下载" class="SIR-main">
-    </ul>
-  `;
+  /**
+   * 获取 svg 图片链接
+   * @param {Elment} svg svg 元素
+   */
+  function getSvgImage(svg) {
+    const image = new Image();
+    image.src =
+      'data:image/svg+xml;base64,' +
+      window.btoa(unescape(encodeURIComponent(svg.outerHTML)));
 
-  document.head.append(style);
-  document.body.append(section);
+    const canvas = document.createElement('canvas');
+    canvas.width = svg.clientWidth;
+    canvas.height = svg.clientHeight;
 
-  const button = section.querySelector('.SIR-toggle-button');
-  const main = section.querySelector('.SIR-main');
+    const context = canvas.getContext('2d');
+    context.drawImage(image, 0, 0);
 
-  // 切换时进行抓取
-  let showMain = false;
-  button.onclick = () => {
-    showMain = !showMain;
-    if (showMain) {
-      imagesReptile();
-      // content
-      let imageList = '';
-      urls.forEach((url) => {
-        imageList += `<li><img src='${url}'</li>`;
-      });
-      svgs.forEach((svg) => {
-        imageList += `<li>${svg}</li>`;
-      });
-      canvas.forEach((canvas) => {
-        imageList += `<li>${canvas}</li>`;
-      });
-      main.innerHTML = imageList;
+    return getCanvasImage(canvas);
+  }
+
+  /**
+   * 获取 canvas 图片链接
+   * @param {Element} canvas canvas 元素
+   */
+  function getCanvasImage(canvas) {
+    return canvas.toDataURL();
+  }
+
+  /**
+   * 获取 img 的链接
+   * @description
+   * 兼容 srcset 属性
+   * @param {Element} element 图片元素
+   */
+  function getImgUrl(element) {
+    let url;
+
+    // 兼容 srcset 属性
+    if (element.srcset) {
+      const srcs = element.srcset.split(',');
+      url = srcs.reduce((pre, curr) => {
+        curr = curr.trim();
+        return curr.includes(' ') ? curr.split(' ')[0] : curr;
+      }, '');
     } else {
-      main.innerHTML = '';
+      url = element.src;
     }
-    main.style.display = showMain ? 'flex' : 'none';
-  };
 
-  // 点击事件委托给父级
-  main.onclick = (e) => {
-    let target = e.target;
-    if (target.tagName === 'LI') {
-      target = target.firstChild;
-    }
-    switch (target.tagName) {
-      case 'IMG':
-        downloadUrl(target.src);
-        break;
-      case 'SVG':
-        downloadSvg(target);
-        break;
-      case 'CANVAS':
-        downloadCanvas(target);
-      default:
-    }
-  };
-}
-
-// 为 url 生成下载链接
-function downloadUrl(url) {
-  var a = document.createElement('a');
-  a.target = '_blank';
-  a.href = url;
-  a.download = 'image';
-  a.click();
-}
-
-// 为 svg 生成下载链接
-function downloadSvg(svg) {
-  const image = new Image();
-  image.src =
-    'data:image/svg+xml;base64,' +
-    window.btoa(unescape(encodeURIComponent(svg.outerHTML)));
-
-  const canvas = document.createElement('canvas');
-  canvas.width = svg.clientWidth;
-  canvas.height = svg.clientHeight;
-
-  const context = canvas.getContext('2d');
-  context.drawImage(image, 0, 0);
-
-  downloadCanvas(canvas);
-}
-
-// 为 canvas 生成下载链接
-function downloadCanvas(canvas) {
-  downloadUrl(canvas.toDataURL());
-}
-
-// 获 img 的链接
-function getImgUrl(element) {
-  let url;
-
-  // 兼容 srcset 属性
-  if (element.srcset) {
-    const srcs = element.srcset.split(',');
-    url = srcs.reduce((pre, curr) => {
-      curr = curr.trim();
-      return curr.includes(' ') ? curr.split(' ')[0] : curr;
-    }, '');
-  } else {
-    url = element.src;
+    return url;
   }
-
-  return url;
-}
+})();

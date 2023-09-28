6f41609425a9fd24d36d3e245858c999a91595e6
yujinpan <1192878390@qq.com>
2023-09-28 14:08:09 +0800

    feat: add hide-img script

---
+++ scripts/hide-img.js
@@ -0,0 +1,16 @@
+// ==UserScript==
+// @name         Hide Image 隐藏图片
+// @namespace    https://github.com/yujinpan/tampermonkey-extension
+// @version      1.0
+// @license      MIT
+// @description  Hide all image in website。
+// @author       yujinpan
+// @include      http*://**
+// @run-at       document-start
+// ==/UserScript==
+
+(function () {
+  const style = document.createElement('style');
+  style.innerHTML = 'img{display:none!important;}*{background-image:none!important;}';
+  document.head.append(style);
+})();
